8d3da98b1338be50abb3939b387a1488219e79ad
yufanana <yufan.fong@gmail.com>
2023-04-14 18:15:49 +0200

    Friday afternoon push

--- Scripts/load_data.py
+++ Scripts/load_data.py
@@ -1,56 +1,48 @@
 import numpy as np
 import pandas as pd
+from os import path
 
 # Load the csv data using the Pandas library
 filename = '../Data/day.csv'
 df = pd.read_csv(filename)
+df = df.drop('instant', axis=1)
+df = df.drop('dteday', axis=1)
 
 
-raw_data = df.values
+# (df['temp']-df['temp'].mean())/df['temp'].std()
+# df.dtypes
+
+X = df.values
 cols = range(0, df.shape[1])
-X = raw_data[:, cols]
+# X = raw_data[:, cols]
 
 # Extract attribute names
 attributeNames = np.asarray(df.columns[cols])
 
 # Check attributes and data shape
 N, M = X.shape
-# print("No. of instances, N: ", N)
-# print("No. of attributes, M: ", M)
-# print("Attribute names: \n", attributeNames)
+print("No. of instances, N: ", N)
+print("No. of attributes, M: ", M)
 
-
-X1 = X
 # undo the original max-min normalization
+temp = df.columns.get_loc("temp")
+atemp = df.columns.get_loc("atemp")
+hum = df.columns.get_loc("hum")
+windspeed = df.columns.get_loc("windspeed")
 for row in range(0, N):
-    X1[row, 9] = X[row, 9]*(39-(-8)) + (-8)
-    X1[row, 10] = X[row, 10]*(50-(-16)) + (-16)
-    X1[row, 11] = X[row, 11]*100
-    X1[row, 12] = X[row, 12]*67
+    X[row, temp] = X[row, temp]*(39-(-8)) + (-8)
+    X[row, atemp] = X[row, atemp]*(50-(-16)) + (-16)
+    X[row, hum] = X[row, hum]*100
+    X[row, windspeed] = X[row, windspeed]*67
 
-X2 = np.empty((731, 7))
 # standarize ratio data attributes for PCA
-X2 = X1[:, 9:16]
-X2 = X2 - np.ones((N, 1))*X2.mean(0)
-X2 = X2*(1/np.std(X2))
+cnt = df.columns.get_loc("cnt")
+# print("mean cnt: ", X[:, cnt].mean(0))
+# print("std cnt: ", np.std(X[:, cnt]))
+for col in range(temp, cnt):
+    # subtract mean, column by column
+    X[:, col] = X[:, col] - np.ones(N) * X[:, col].mean(0)
+    X[:, col] = X[:, col] * (1/np.std(X[:, cnt]))
 
-# concatatenate the 2 arrays
-Y = np.concatenate((X1[:, 0:9], X2), axis=1)
-
-# Check attributes and data shape
-N, M = X2.shape
-# print("No. of instances, N: ", N)
-# print("No. of attributes, M: ", M)
-
-for i in range(N):
-    for j in range(M):
-        X2[i, j] = float(X2[i, j])
-
-# print(type(X2[0, 0]))
-# print(type(X2[0, 1]))
-# print(type(X2[0, 2]))
-# print(type(X2[0, 3]))
-# print(type(X2[0, 4]))
-# print(type(X2[0, 5]))
-# print(type(X2[0, 6]))
-print("Finished running load_data.py")
+# Show successful code execution
+print("Finished running", path.basename(__file__))
--- Scripts/run_pca.py
+++ Scripts/run_pca.py
@@ -1,32 +1,49 @@
 # Use data structure from load_data.py
 from load_data import *
+from os import path
 
 
-from scipy.io import loadmat
 from scipy.linalg import svd
 import matplotlib.pyplot as plt
 
+temp = df.columns.get_loc("temp")
+X = X[:, temp:]
+
 # PCA by computing SVD
-U, S, V = svd(X2, full_matrices=False)
+U, S, V = svd(X, full_matrices=False)
 
 # Compute variance explained by principal components
 rho = (S*S) / (S*S).sum()
 
+# Project data onto principal component space
+Z = X @ V
+
 # from ex2_1_3.py
 
-# threshold_1 = 0.9
-# threshold_2 = 0.95
 # Plot variance explained
 plt.figure()
 plt.plot(range(1, len(rho)+1), rho, 'x-')
 plt.plot(range(1, len(rho)+1), np.cumsum(rho), 'o-')
-# plt.plot([1, len(rho)], [threshold_1, threshold_1], 'k--')
-# plt.plot([1, len(rho)], [threshold_2, threshold_2], 'y--')
 plt.title('Variance explained by principal components')
 plt.xlabel('Principal component')
 plt.ylabel('Variance explained')
-plt.legend(['Individual', 'Cumulative', '90% Threshold', '95% Threshold'])
+plt.legend(['Individual', 'Cumulative'])
 plt.grid()
 plt.show()
 
-print("Finished running run_pca.py")
+# Plot scatter diagram
+plt.figure()
+plt.title('Bike data')
+
+for c in range(C):
+    # select indices belonging to class c:
+    class_mask = y == c
+    plt.plot(X[class_mask, i], X[class_mask, j], 'o', alpha=.3)
+
+# plt.legend(attributeNames)
+plt.xlabel(attributeNames[i])
+plt.ylabel(attributeNames[j])
+plt.show()
+
+# Show successful code execution
+print("Finished running", path.basename(__file__))
